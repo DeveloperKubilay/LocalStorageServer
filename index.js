88a4b104f9d327193222c38000e76d33d1d8f895
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-01-08 01:51:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = require('express')()
 fs = require('fs')
 app.listen(3000)
 
-try{fs.readdirSync("./files")}catch{fs.mkdirSync("./files")}
+try{fs.readdirSync('./files')}catch{fs.mkdirSync('./files')}
 app.get('/file/:file', function (req, res) {
  if(fs.existsSync('./files/'+req.params.file)) {
- res.download("./files/"+req.params.file)
+ res.download('./files/'+req.params.file)
  }else return res.end();
 })
