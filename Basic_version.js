5fedac4d8f55664217d53dac9812a8dd39f2adbe
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-06 19:17:40 +0300

    new v

---
+++ Basic_version.js
@@ -0,0 +1,13 @@
+var port = 3000
+
+const app = require('express')()
+fs = require('fs')
+app.listen(port)
+console.log("Running")
+
+try { fs.readdirSync('./files') } catch { fs.mkdirSync('./files') }
+app.get('/file/:file', function (req, res) {
+    if (fs.existsSync('./files/' + req.params.file)) {
+        res.download('./files/' + req.params.file)
+    } else return res.end();
+})
